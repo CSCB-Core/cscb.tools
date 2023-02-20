5ac3256a24493dfd1c48aebd91430fa7628652cf
Seyedian <arta.seyedian@gmail.com>
2023-02-20 11:39:41 -0500

    added feature to automatically convert any column bearing the name 'cluster' to categorical - if it ends up being an issue, we should add a condition that it's an integer-only column

--- inst/python/fix_cellxgene.py
+++ inst/python/fix_cellxgene.py
@@ -1,11 +1,16 @@
 # from https://github.com/theislab/scvelo/issues/255#issuecomment-739995301
 import scanpy as sc
+import pandas as pd
 
-def fix_cellxgene(file, file_out = None):
-  file_out = file if file_out is None else file_out
-  scrna_adata = sc.read_h5ad(file)
-  # for reading into cellxgene
-  scrna_adata.var_names = scrna_adata.var.features
-  scrna_adata.__dict__['_raw'].__dict__['_var'] = scrna_adata.__dict__['_raw'].__dict__['_var'].rename(columns={'_index': 'features'})
-  del(scrna_adata.var['_index'])
-  scrna_adata.write_h5ad(file_out)
+def fix_cellxgene(file, file_out=None):
+    file_out = file if file_out is None else file_out
+    scrna_adata = sc.read_h5ad(file)
+    # find all columns containing the word "cluster"
+    cluster_cols = scrna_adata.obs.filter(regex='cluster').columns
+    # convert all cluster metadata columns to categorical variables
+    scrna_adata.obs[cluster_cols] = scrna_adata.obs[cluster_cols].astype('category')
+    # for reading into cellxgene
+    scrna_adata.var_names = scrna_adata.var.features
+    scrna_adata.__dict__['_raw'].__dict__['_var'] = scrna_adata.__dict__['_raw'].__dict__['_var'].rename(columns={'_index': 'features'})
+    del(scrna_adata.var['_index'])
+    scrna_adata.write_h5ad(file_out)
