038ab68d4c5e7b784a4c2920874792c86286c6ed
Seyedian <arta.seyedian@gmail.com>
2023-05-19 13:58:45 -0400

    fixing fix_cellxgene.py

--- inst/python/fix_cellxgene.py
+++ inst/python/fix_cellxgene.py
@@ -12,5 +12,5 @@ def fix_cellxgene(file, file_out=None):
     # for reading into cellxgene
     scrna_adata.var_names = scrna_adata.var.features
     scrna_adata.__dict__['_raw'].__dict__['_var'] = scrna_adata.__dict__['_raw'].__dict__['_var'].rename(columns={'_index': 'features'})
-    del(scrna_adata.var['_index'])
+    # del(scrna_adata.var['_index'])
     scrna_adata.write_h5ad(file_out)
